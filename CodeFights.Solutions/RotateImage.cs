0e17696fdb4f89b349b15b05baf2ce49664fb82a
Felipe Jaramillo <pipeluck@gmail.com>
2018-05-22 17:33:42 -0500

    add rotate image

---
+++ CodeFights.Solutions/RotateImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+   Note: Try to solve this task in-place (with O(1) additional memory), since this is what you'll be asked to do during an interview.
+   
+   You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
+   
+   Example
+   
+   For
+   
+   a = [[1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9]]
+   the output should be
+   
+   rotateImage(a) =
+   [[7, 4, 1],
+   [8, 5, 2],
+   [9, 6, 3]]
+ */
+namespace CodeFights.Solutions
+{
+    public class RotateImage
+    {
+        public static int[][] rotateImage(int[][] a)
+        {
+            var newMatrix = new int[a.Length][];
+            var matrixSize = a.Length - 1;
+            for (var rowIndex = 0; rowIndex < a.Length; rowIndex++)
+            {
+                newMatrix[rowIndex] = new int[a.Length];
+                for (var columnIndex = 0; columnIndex < a.Length; columnIndex++)
+                {
+                    var newRowIndex = matrixSize - columnIndex;
+                    var newColumnIndex = rowIndex;
+                    newMatrix[rowIndex][columnIndex] = a[newRowIndex][newColumnIndex];
+                }
+            }
+ 
+            return newMatrix.ToArray();
+
+        }
+    }
+}
---
+++ CodeFights/RotateImageTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using CodeFights.Solutions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeFights.Tests
+{
+    [TestClass]
+    public class RotateImageTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var input = new[] { new[] { 1, 2, 3 },
+                                new[] { 4, 5, 6 },
+                                new[] { 7, 8, 9 } };
+
+            var result = RotateImage.rotateImage(input);
+
+            var expectedResult = new[] { new[] { 7, 4, 1 },
+                new[] { 8, 5, 2},
+                new[] { 9, 6, 3 } };
+
+            CollectionAssert.AreEqual(expectedResult, result, new CollectionAssertComperator());
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var input = new[] { new[] { 1 } };
+
+            var result = RotateImage.rotateImage(input);
+
+            var expectedResult = new[] { new[] { 1 } };
+
+            CollectionAssert.AreEqual(expectedResult, result, new CollectionAssertComperator());
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var input = new[] { new[] {10, 9, 6, 3, 7},
+                new[] { 6, 10, 2, 9, 7 },
+                new[] { 7, 6, 3, 8, 2 },
+                new[] { 8, 9, 7, 9, 9},
+                new[] { 6, 8, 6, 8, 2},
+            };
+
+            var result = RotateImage.rotateImage(input);
+
+            var expectedResult = new[] { new[] {6, 8, 7, 6, 10},
+                new[] { 8, 9, 6, 10, 9 },
+                new[] { 6, 7, 3, 2, 6 },
+                new[] { 8, 9, 8, 9, 3},
+                new[] { 2, 9, 2, 7, 7},
+            };
+
+
+            CollectionAssert.AreEqual(expectedResult, result, new CollectionAssertComperator());
+        }
+    }
+
+    class CollectionAssertComperator : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            CollectionAssert.AreEqual((ICollection)x, (ICollection)y);
+            return 0;
+        }
+    }
+}
