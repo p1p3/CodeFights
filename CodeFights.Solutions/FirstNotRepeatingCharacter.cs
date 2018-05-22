97895aae88755e76058ad9e23f10055674e7a63f
Felipe Jaramillo <pipeluck@gmail.com>
2018-05-22 08:58:34 -0500

    add FirstNotRepeatingCharacterTests

---
+++ CodeFights.Solutions/FirstNotRepeatingCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/* Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
+
+Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+Example
+
+For s = "abacabad", the output should be
+firstNotRepeatingCharacter(s) = 'c'.
+
+There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+For s = "abacabaabacaba", the output should be
+firstNotRepeatingCharacter(s) = '_'.
+
+There are no characters in this string that do not repeat.*/
+
+namespace CodeFights.Solutions
+{
+    public static class FirstNotRepeatingCharacter
+    {
+        public static char firstNotRepeatingCharacter(string s)
+        {
+            var chars = s.ToCharArray();
+
+            var groupedChars = chars.GroupBy(c => c);
+            var notRepeated = groupedChars.Where(group => group.Count() == 1).ToList();
+
+            return notRepeated.Any() ? Convert.ToChar(notRepeated.First().Key) : '_';
+        }
+    }
+}
---
+++ CodeFights/FirstNotRepeatingCharacterTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeFights.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeFights.Tests
+{
+    [TestClass]
+    public class FirstNotRepeatingCharacterTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var result = FirstNotRepeatingCharacter.firstNotRepeatingCharacter("abacabad");
+            Assert.AreEqual('c', result);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var result = FirstNotRepeatingCharacter.firstNotRepeatingCharacter("abacabaabacaba");
+            Assert.AreEqual('_', result);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var result = FirstNotRepeatingCharacter.firstNotRepeatingCharacter("z");
+            Assert.AreEqual('z', result);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var result = FirstNotRepeatingCharacter.firstNotRepeatingCharacter("bcb");
+            Assert.AreEqual('c', result);
+        }
+    }
+}
